32303913509679653609c13fa68653439b26b115
Flavio Costa <skingk.f@gmail.com>
2017-09-06 11:18:07 -0300

    Refactor random limits
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>

--- mobile/src/components/turbine-data/turbine-data.ts
+++ mobile/src/components/turbine-data/turbine-data.ts
@@ -14,26 +14,26 @@ export class TurbineDataComponent implements OnDestroy {
     this.turbine_datas =
       [
         {
-          'image_src': 'assets/img/wind.jpg',
+          'image_src': 'assets/img/wind.png',
           'title': 'Velocidade do Vento',
           'subtitle': 4,
           'unity': ' m/s'
         },
         {
-          'image_src': 'assets/img/voltagem.jpg',
-          'title': 'Voltagem da Turbina',
+          'image_src': 'assets/img/volt.png',
+          'title': 'Tensão',
           'subtitle': Math.floor(Math.random() * 220),
           'unity': ' V'
         },
         {
-          'image_src': 'assets/img/tensao.jpg',
-          'title': 'Tensão',
+          'image_src': 'assets/img/tension.png',
+          'title': 'Corrente',
           'subtitle': 10,
           'unity': ' A'
         },
         {
-          'image_src': 'assets/img/MPPT.png',
-          'title': 'MPPT',
+          'image_src': 'assets/img/mppt.png',
+          'title': 'Potência',
           'subtitle': 10,
           'unity': ' W'
         },
@@ -54,7 +54,24 @@ export class TurbineDataComponent implements OnDestroy {
   setUpdateData() {
     setInterval(() => {
       this.turbine_datas.forEach((item) => {
-        item['subtitle'] = Math.floor(Math.random() * 220) + item['unity'];
+        var wind = Math.floor(Math.random() * (9 - 1) + 1);
+        var tension = Math.floor(Math.random() * (150 - 1) + 1);
+        var flow = Math.floor(Math.random() * (10 - 1) + 1);
+        var power = tension * flow;
+        console.log(flow + ' x ' + tension + " = " + power);
+        
+        if (item['title'] === 'Velocidade do Vento') {
+          item['subtitle'] = wind + item['unity'];
+        } else
+        if (item['title'] === 'Tensão') {
+          item['subtitle'] = tension + item['unity'];
+        } else
+        if (item['title'] === 'Corrente') {
+          item['subtitle'] = flow + item['unity'];
+        } else 
+        if (item['title'] === 'Potência') {
+          item['subtitle'] = power + item['unity'];
+        }
       });
     }, 2000);
   }
