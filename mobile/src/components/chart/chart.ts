b1ffa8f3cb867202d1bce6e883fe9cb61095b1ba
Flavio Costa <skingk.f@gmail.com>
2017-09-13 17:09:23 -0300

    Create chart components
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>

---
+++ mobile/src/components/chart/chart.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the ChartComponent component.
+ *
+ * See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
+ * for more info on Angular Components.
+ */
+@Component({
+  selector: 'chart',
+  templateUrl: 'chart.html'
+})
+export class ChartComponent {
+
+  text: string;
+  public lineChartData:Array<any> = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+
+  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartType:string = 'line';
+  public pieChartType:string = 'pie';
+ 
+  // Pie
+  public pieChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+  public pieChartData:number[] = [300, 500, 100];
+ 
+  public randomizeType():void {
+    this.lineChartType = this.lineChartType === 'line' ? 'bar' : 'line';
+    this.pieChartType = this.pieChartType === 'doughnut' ? 'pie' : 'doughnut';
+  }
+ 
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+ 
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
+}
