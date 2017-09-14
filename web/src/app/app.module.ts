ad3ccc853ee558dfedfcb4104b5abd59bb4f5be6
Antonio Carvalho <_antonio_@hotmail.com>
2017-09-14 20:19:16 -0300

    Add web cards
    Signed-off-by: Antonio Carvalho <_antonio_@hotmail.com>

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { TurbineDataComponent } from './components/turbine-data/turbine-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    TurbineDataComponent
   ],
   imports: [
     BrowserModule
