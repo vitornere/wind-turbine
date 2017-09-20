7846714d130d02c53137fec236edb339739a2fba
Antonio Carvalho <_antonio_@hotmail.com>
2017-09-20 17:38:43 -0300

    Redifine nav-bar
    Signed-off-by: Antonio Carvalho <_antonio_@hotmail.com>

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { TurbineDataComponent } from './components/turbine-data/turbine-data.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    TurbineDataComponent
+    TurbineDataComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
