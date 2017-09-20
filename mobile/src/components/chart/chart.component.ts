7d08bf151a549b987ace6dc9159d06080a67ac9f
Jonnatas <jonatas_lenon@hotmail.com>
2017-09-19 22:07:55 -0300

    add update of labels in chart

--- mobile/src/components/chart/chart.component.ts
+++ mobile/src/components/chart/chart.component.ts
@@ -15,42 +15,39 @@ export class ChartComponent {
   today = new Date();
 
   @Input()
-  initDate: number = this.today.getFullYear();
-  @Input()  
-  finalDAte: number = this.today.getFullYear();
+  initYear: number = this.today.getFullYear();
+  @Input()
+  finalYear: number = this.today.getFullYear();
 
-  public lineChartData:Array<any> = [
-    { data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56,], label: this.initDate },
-    { data: [65, 52, 30, 11, 36, 51, 12, 63, 59, 80, 81, 10,], label: this.finalDAte },
+  public lineChartData: Array<any> = [
+    { data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], label: this.initYear },
+    { data: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,], label: this.finalYear },
   ];
 
-  public lineChartLabels:Array<any> = ['Janeiro','Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
-                                      'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
-  public lineChartType:string = 'line';
+  public lineChartLabels: Array<any> = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+  public lineChartType: string = 'line';
 
-  public randomize():void {
-    let lineChartData:Array<any> = new Array(this.lineChartData.length);
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+  public randomize(): void {
+    let lineChartData: Array<any> = new Array(this.lineChartData.length);
+    
     for (let i = 0; i < this.lineChartData.length; i++) {
-      lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
+      lineChartData[i] = { data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label };
       for (let j = 0; j < this.lineChartData[i].data.length; j++) {
         lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
       }
     }
-    console.log('aq')
+    lineChartData[0].label = this.initYear;
+    lineChartData[1].label = this.finalYear;
+    console.log(lineChartData[0]);
     this.lineChartData = lineChartData;
   }
- 
 
-  constructor() {
-    console.log(this.initDate); 
-    console.log(this.initDate.toString)   
-  }
-  public chartClicked(e:any):void {
-    console.log(e);
-  }
- 
-  public chartHovered(e:any):void {
-    console.log(e);
-  }
-
-}
+} 
