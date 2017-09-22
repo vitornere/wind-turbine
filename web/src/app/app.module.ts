c2fe139396afeecdf1b959d206e04dd6db789761
Antonio Carvalho <_antonio_@hotmail.com>
2017-09-22 16:02:54 -0300

    Adding JQuery
    Signed-off-by: Antonio Carvalho <_antonio_@hotmail.com>

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { TurbineDataComponent } from './components/turbine-data/turbine-data.component';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component'
 
 @NgModule({
   declarations: [
