d117a2c078ca1d814ad2a01dcb6f3c63a48c0a23
Jonnatas <jonatas_lenon@hotmail.com>
2017-09-22 15:57:47 -0300

    fixing bug values in data field

--- mobile/src/components/chart/chart.component.ts
+++ mobile/src/components/chart/chart.component.ts
@@ -18,32 +18,33 @@ export class ChartComponent {
   lowStatMonth: string;
   defaultHigh = 0;
   defaultLow = 220;
+  highStat: number = this.defaultHigh;
+  lowStat: number = this.defaultLow;
 
   @Input()
   initYear: number = this.today.getFullYear();
   @Input()
   finalYear: number = this.today.getFullYear();
 
-  firstYear = { 
-    data: [5, 2, 30000, 4, 5, 6, 7, 8, 9, 10, 11, 12], 
-    label: this.initYear 
-  }  
-  
-  secondYear = { 
-    data: [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 12,33], 
-    label: this.finalYear 
-  }  
-  
+  firstYear = {
+    data: [5, 25000, 30000, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    label: this.initYear
+  }
+
+  secondYear = {
+    data: [1230000, 1130000, 30000, 930000, 830000, 730000, 630000, 530000, 430000, 330000, 1230000, 3330000],
+    label: this.finalYear
+  }
+
   public lineChartData: Array<any> = [
     this.firstYear,
     this.secondYear
   ];
 
-  public lineChartLabels: Array<any> = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
-    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+  public lineChartLabels: Array<any> = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
   public lineChartType: string = 'line';
 
-  
+
   public chartHovered(e: any): void {
     console.log(e);
   }
@@ -51,26 +52,19 @@ export class ChartComponent {
   public chartClicked(e: any): void {
     console.log(e);
   }
-
-  highStat: number = this.defaultHigh;
-  lowStat: number = this.defaultLow;
-
   public statsChart(year: any): void {
 
-    for (let i = 0; i < year.data.length; i++){
-      if(year.data[i]>this.highStat){
+    for (let i = 0; i < year.data.length; i++) {
+      if (year.data[i] > this.highStat) {
         this.highStat = year.data[i];
         this.highStatMonth = this.lineChartLabels[i];
-                 
+
       }
-      if(year.data[i]<this.lowStat){
+      if (year.data[i] < this.lowStat) {
         this.lowStat = year.data[i];
-        this.lowStatMonth = this.lineChartLabels[i];  
+        this.lowStatMonth = this.lineChartLabels[i];
       }
-      console.log('high - ' + this.highStat + " mes " + this.lineChartLabels[i]);
-      console.log('low - ' + this.lowStat + " mes " + this.lineChartLabels[i]);   
     }
-   
   }
 
   public updateYear(): void {
@@ -79,19 +73,24 @@ export class ChartComponent {
     for (let i = 0; i < this.lineChartData.length; i++) {
       lineChartData[i] = { data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label };
       for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
+        lineChartData[i].data[j] = Math.floor((Math.random() * 300) + 1);
       }
     }
     lineChartData[0].label = this.initYear;
     lineChartData[1].label = this.finalYear;
-    
-    this.firstYear = this.lineChartData[0];
+
+    this.firstYear = lineChartData[0];
+    this.secondYear = lineChartData[1];
+
     this.lineChartData = lineChartData;
+
     this.highStat = this.defaultHigh;
     this.lowStat = this.defaultLow;
+
     this.statsChart(this.firstYear);
     this.statsChart(this.secondYear);
-    this.showChart=true;
+
+    this.showChart = true;
   }
-  
+
 } 
