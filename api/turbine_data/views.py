f638330c1f640ebc7884209ea3aa6c4fae98c739
Vitor Nere <vitor.nere@hotmail.com>
2017-09-27 18:05:26 -0300

    Created getLastTurbineData
    Signed-off-by: Vitor Nere <vitor.nere@hotmail.com>
    Signed-off-by: Jonnatas Lennon <jonatas_lenon@hotmail.com>

--- api/turbine_data/views.py
+++ api/turbine_data/views.py
@@ -1,10 +1,24 @@
 from rest_framework import viewsets
 from .models import TurbineData
 from .serializers import TurbineDataSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
+@api_view(['GET'])
+def getLastTurbineData(response):
+        turbineData = TurbineData.objects.last()
+
+        if turbineData is not None:
+            serialize = TurbineDataSerializer(turbineData)
+
+            return Response(serialize.data, status.HTTP_200_OK)
+        else:
+            return Response({}, status.HTTP_200_OK)
+    
 class TurbineDataViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allow TurbineData to be viewed or edited.
     """
-    queryset = TurbineData.objects.all().order_by('date')
+    queryset = TurbineData.objects.all()
     serializer_class = TurbineDataSerializer
