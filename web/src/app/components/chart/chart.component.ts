633dd12a23ddb71ffa417d6976c9ddb5146584fe
Vitor Nere <vitor.nere@hotmail.com>
2017-09-29 17:31:06 -0300

    Resizing chart height

--- web/src/app/components/chart/chart.component.ts
+++ web/src/app/components/chart/chart.component.ts
@@ -3,6 +3,10 @@ import { Component, OnDestroy, AfterViewInit, ViewChild, ElementRef } from '@ang
 const Highcharts = require('highcharts/highcharts.src');
 import 'highcharts/adapters/standalone-framework.src';
 
+Highcharts.setOptions({
+  height: ['300px']
+});
+
 @Component({
   selector: 'app-chart',
   template: `<div><div #chart></div></div>`,
@@ -11,15 +15,15 @@ import 'highcharts/adapters/standalone-framework.src';
 })
 export class ChartComponent implements AfterViewInit, OnDestroy {
   @ViewChild('chart') public chartEl: ElementRef;
-  
+
   private _chart: any;
-  
+
   private randomValue() {
     return Math.floor(Math.random() * 10) + 0;
   }
-  
+
   public ngAfterViewInit() {
-    let opts: any = {
+    const opts: any = {
         xAxis: {
           type: 'datetime',
           tickPixelInterval: 150
@@ -28,10 +32,8 @@ export class ChartComponent implements AfterViewInit, OnDestroy {
           name: 'Random data',
           data: (function () {
               // generate an array of random data
-              var data = [],
-                  time = (new Date()).getTime(),
-                  i;
-
+              let data = [],
+                  time = (new Date()).getTime(), i;
               for (i = -19; i <= 0; i += 1) {
                   data.push({
                       x: time + i * 1000,
@@ -42,24 +44,25 @@ export class ChartComponent implements AfterViewInit, OnDestroy {
           }())
         }]
     };
-    
+
     if (this.chartEl && this.chartEl.nativeElement) {
         opts.chart = {
             type: 'spline',
-            renderTo: this.chartEl.nativeElement
+            renderTo: this.chartEl.nativeElement,
+            height: 300
         };
 
         this._chart = new Highcharts.Chart(opts);
     }
   }
-  
+
   public ngOnDestroy() {
     this._chart.destroy();
   }
-  
+
   constructor() {
     const me = this;
-    
+
     setInterval(function () {
       if (me._chart) {
         me._chart['series'][0].addPoint([(new Date()).getTime(), me.randomValue()], true, true);
