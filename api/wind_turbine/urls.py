5e160951ecebf92ed20f7a89e9acc95e14b23f9c
Jonnatas <jonatas_lenon@hotmail.com>
2017-09-29 17:15:28 -0300

    add search data by date

--- api/wind_turbine/urls.py
+++ api/wind_turbine/urls.py
@@ -7,5 +7,6 @@ router.register(r'turbine-data', views.TurbineDataViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls), name='turbine-data'),
-    url(r'^turbine-data/last', views.getLastTurbineData, name='turbine-data-last')
+    url(r'^turbine-data/last', views.getLastTurbineData, name='turbine-data-last'),
+    url(r'^start:(?P<start_date>\d{4})&&finish:(?P<finish_date>\d{4})/$', views.getTurbineDataByDate, name='turbine-data-last'),
 ]
