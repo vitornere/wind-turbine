7d94eaa7783a9965ae2c9e9636ac9f3ede623c22
Jonnatas <jonatas_lenon@hotmail.com>
2017-10-04 17:44:51 -0300

    create get of month and year

--- api/wind_turbine/urls.py
+++ api/wind_turbine/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^', include(router.urls), name='turbine-data'),
     url(r'^turbine-data/last', views.getLastTurbineData, name='turbine-data-last'),
     url(r'^start:(?P<start_year>\d{4})-(?P<start_month>\d{2})-(?P<start_day>\d{2})&&finish:(?P<finish_year>\d{4})-(?P<finish_month>\d{2})-(?P<finish_day>\d{2})/$', views.getTurbineDataByCompleteDate, name='turbine-data-last'),
+    url(r'^start_year:(?P<start_year>\d{4})&&finish_year:(?P<finish_year>\d{4})/$', views.getTurbineDataByYear, name='turbine-data-last'),
+    url(r'^year:(?P<year>\d{4})&&start_month:(?P<start_month>\d{2})&&finish_month:(?P<finish_month>\d{2})/$', views.getTurbineDataByMonth, name='turbine-data-last'),
 ]
