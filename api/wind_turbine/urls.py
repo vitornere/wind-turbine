7671e354e682304d2094193bc34292f35cceb3ae
Jonnatas <jonatas_lenon@hotmail.com>
2017-10-04 17:09:25 -0300

    add method Get for search all dates in especific interval

--- api/wind_turbine/urls.py
+++ api/wind_turbine/urls.py
@@ -8,5 +8,5 @@ router.register(r'turbine-data', views.TurbineDataViewSet)
 urlpatterns = [
     url(r'^', include(router.urls), name='turbine-data'),
     url(r'^turbine-data/last', views.getLastTurbineData, name='turbine-data-last'),
-    url(r'^start:(?P<start_year>\d{4})-(?P<start_month>\d{2})-(?P<start_day>\d{2})&&finish:(?P<finish_year>\d{4})-(?P<finish_month>\d{2})-(?P<finish_day>\d{2})/$', views.getTurbineDataByDate, name='turbine-data-last'),
+    url(r'^start:(?P<start_year>\d{4})-(?P<start_month>\d{2})-(?P<start_day>\d{2})&&finish:(?P<finish_year>\d{4})-(?P<finish_month>\d{2})-(?P<finish_day>\d{2})/$', views.getTurbineDataByCompleteDate, name='turbine-data-last'),
 ]
