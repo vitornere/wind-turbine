9d357694b42ef70c2113f3dbc1ca391cfe4efb55
Jonnatas <jonatas_lenon@hotmail.com>
2017-10-23 18:26:58 -0200

    resolve material problems

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,11 +1,11 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { TurbineDataService } from './services/turbine-data.service';
 import { CustomMaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {NoConflictStyleCompatibilityMode} from '@angular/material';
 import { AppComponent } from './app.component';
 import { TurbineDataComponent } from './components/turbine-data/turbine-data.component';
 import { ChartComponent } from './components/chart/chart.component';
@@ -27,6 +27,7 @@ import { HttpModule } from '@angular/http';
     HomeComponent
   ],
   imports: [
+    NoConflictStyleCompatibilityMode,
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
