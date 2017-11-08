646c834cb411330827dacdbac00590b10fd9bb1b
Vitor Nere <vitor.nere@hotmail.com>
2017-11-08 15:51:14 -0200

    Added mask to datas
    Signed-off-by: Vitor Nere <vitor.nere@hotmail.com>

--- web/src/app/components/turbine-data/turbine-data.component.ts
+++ web/src/app/components/turbine-data/turbine-data.component.ts
@@ -15,9 +15,9 @@ export class TurbineDataComponent implements OnInit {
   constructor(private turbineDataService: TurbineDataService) {
     this.turbine_data = [
       this.newTurbineDataObject(0, 'assets/img/wind.png', 'Velocidade do Vento', '', ' m/s'),
-      this.newTurbineDataObject(1, 'assets/img/volt.png', 'Tensão', '', ' m/s'),
-      this.newTurbineDataObject(2, 'assets/img/tension.png', 'Corrente', '', ' m/s'),
-      this.newTurbineDataObject(3, 'assets/img/mppt.png', 'Máxima Potência', '', ' m/s')
+      this.newTurbineDataObject(1, 'assets/img/volt.png', 'Tensão', '', ' v'),
+      this.newTurbineDataObject(2, 'assets/img/tension.png', 'Corrente', '', ' A'),
+      this.newTurbineDataObject(3, 'assets/img/mppt.png', 'Máxima Potência', '', ' W')
     ];
     this.updateData = this.setUpdateData();
   }
