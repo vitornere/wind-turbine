d11cf2838afe0bf737d9e0acac73b27182358054
Jonnatas <jonatas_lenon@hotmail.com>
2017-11-08 02:53:57 -0200

    creating dowload im mobile

---
+++ mobile/src/models/element-table.model.ts
@@ -0,0 +1,21 @@
+export class ElementTableModel {
+    id: number;
+    date: string;
+    wind_speed: string;
+    electric_voltage: string;
+    electric_current: string;
+    mppt: string;
+
+    constructor(id: number, date: string, wind_speed: string, electric_voltage: string, electric_current: string, mppt: string) {
+        id = this.id;
+        date = this.date;
+        wind_speed = this.wind_speed;
+        electric_voltage = this.electric_voltage;
+        electric_current = this.electric_current;
+        mppt = this.mppt;
+    }
+
+    public setDate(date: string): void {
+        this.date = date;
+    }
+}
--- mobile/src/pages/history/history.ts
+++ mobile/src/pages/history/history.ts
@@ -1,10 +1,12 @@
-import { ElementTableModel } from './../../../../web/src/app/models/element-table.models';
 import { TurbineDataService } from './../../providers/turbine-data-service/turbine-data-service';
+import { ElementTableModel } from './../../../../web/src/app/models/element-table.models';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { Angular2Csv } from 'angular2-csv/Angular2-csv';
 import { AlertController } from 'ionic-angular';
+
+import 'rxjs/Rx';
 
 @Component({
   selector: 'page-history',
@@ -25,7 +27,7 @@ export class HistoryPage {
   showPeriodo: boolean = false;
   frequencia: boolean = false;
   downloadButton: boolean
-
+  noData: boolean;
   frequency
 
   private elements_model: ElementTableModel[];
@@ -36,11 +38,33 @@ export class HistoryPage {
   }
 
   radioButton() {
+    if (this.frequency === undefined) {
+      this.showAlert('Selecione a frequência dos dados');
+      this.downloadButton = false;
+    } else {
+      this.turbineDataService.getTurbineDataByCompleteDate(
+        this.frequency,
+        this.displayedColumns,
+        this.timeStarts,
+        this.timeEnds
+      ).retry(3)
+        .subscribe(
+        res => {
+          this.elements_model = res;
+          if (this.elements_model.length > 1) {
+            this.noData = false;
+            this.format;
+            this.downloadButton = true;
+          } else {
+            this.noData = true;
+          }
+        });
+    }
     console.log(this.frequency);
   }
 
   datetimeButton() {
-    if (this.timeStarts === undefined || this.timeEnds === undefined){
+    if (this.timeStarts === undefined || this.timeEnds === undefined) {
       this.showAlert('Você esqueceu de selecionar as datas');
     } else {
       this.frequencia = true;
@@ -51,21 +75,47 @@ export class HistoryPage {
       this.showAlert('Selecione pelo menos um item');
       this.showPeriodo = false;
     } else {
+
+      if (this.wind_speed === true && this.displayedColumns.indexOf('wind_speed') === -1) {
+        this.displayedColumns.push('wind_speed');
+      } else if (this.wind_speed === false && this.displayedColumns.indexOf('wind_speed') >= 0) {
+        const indexV = this.displayedColumns.indexOf('wind_speed');
+        this.displayedColumns.splice(indexV, 1);
+      }
+
+      if (this.electric_voltage === true && this.displayedColumns.indexOf('electric_voltage') === -1) {
+        this.displayedColumns.push('electric_voltage');
+      } else if (this.electric_voltage === false && this.displayedColumns.indexOf('electric_voltage') >= 0) {
+        const idx = this.displayedColumns.indexOf('electric_voltage');
+        this.displayedColumns.splice(idx, 1);
+      }
+
+      if (this.electric_current === true && this.displayedColumns.indexOf('electric_current') === -1) {
+        this.displayedColumns.push('electric_current');
+      } else if (this.electric_current === false && this.displayedColumns.indexOf('electric_current') >= 0) {
+        const idx = this.displayedColumns.indexOf('electric_current');
+        this.displayedColumns.splice(idx, 1);
+      }
+
+      if (this.mppt === true && this.displayedColumns.indexOf('mppt') === -1) {
+        this.displayedColumns.push('mppt');
+      } else if (this.mppt === false && this.displayedColumns.indexOf('mppt') >= 0) {
+        const idx = this.displayedColumns.indexOf('mppt');
+        this.displayedColumns.splice(idx, 1);
+      }
+
       this.showPeriodo = true;
     }
   }
   showAlert(msg: string) {
     let alert = this.alertCtrl.create({
-      title: 'Erro!',
+      title: 'Opps!',
       subTitle: msg,
       buttons: ['OK']
     });
     alert.present();
   }
 
-  ionViewDidLoad() {
-    console.log('Na página HistoryPage');
-  }
   download() {
     const options = {
       fieldSeparator: ';',
@@ -79,6 +129,14 @@ export class HistoryPage {
       this.elements_model
       , 'turbine_data'
       , options
+    );
+  }
+
+  format(): void {
+    this.elements_model.map(
+      res => {
+        res.date = res.date.slice(0, 19).replace('T', ' ');
+      }
     );
   }
 
