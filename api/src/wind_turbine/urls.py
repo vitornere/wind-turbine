8c292369152dd45c2aa2fa21f0207c1999a4869c
Jonnatas <jonatas_lenon@hotmail.com>
2017-11-22 22:47:11 -0200

    add filter of date year

--- api/src/wind_turbine/urls.py
+++ api/src/wind_turbine/urls.py
@@ -13,22 +13,13 @@ urlpatterns = [
         name='turbine-data-last'
     ),
     url(
-        r'^start_year:(?P<start_year>\d{4})&&finish_year:(?P<finish_year>\d{4})/$', 
+        r'^start_year:(?P<start_year>\d{4})&&finish_year:(?P<finish_year>\d{4})::(?P<selected_value>[\w]+)/$', 
         views.getTurbineDataByYear, 
         name='turbine-data-year'
-    ),
-    url(
-        r'^year:(?P<year>\d{4})&&start_month:(?P<start_month>\d{1,2})&&finish_month:(?P<finish_month>\d{1,2})/$', views.getTurbineDataByMonth, 
-        name='turbine-data-month'
     ),
     url(
         r'^period:(?P<period>\w+)&&start:(?P<start_date>[-\w]+)&&finish:(?P<finish_date>[-\w]+)::(?P<selected_values>[,\w]+)/$', 
         views.getTurbineDataByCompleteDate, 
         name='turbine-data-complete'
-    ),
-    url(
-        r'^start:(?P<start_year>\d{4})-(?P<start_month>\d{1,2})-(?P<start_day>\d{1,2})/$', 
-        views.getTurbineDataByDay, 
-        name='turbine-data-day'
     ),
 ]
