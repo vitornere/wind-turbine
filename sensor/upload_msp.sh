26c751d8e02bd0d1cff0a89b2d41efd61812fd2d
Vitor Nere <vitor.nere@hotmail.com>
2017-11-30 16:22:05 -0200

    Trying to read from sensor with msp

--- sensor/upload_msp.sh
+++ sensor/upload_msp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-msp430-gcc -Os -Wall -g -mmcu=msp430g2553 -o send_string_by_usb.elf send_string_by_usb.c
-sudo mspdebug rf2500 "prog send_string_by_usb.elf"
-rm send_string_by_usb.elf
+msp430-gcc -Os -Wall -g -mmcu=msp430g2553 -o uart3.elf uart3.c
+sudo mspdebug rf2500 "prog uart3.elf"
+rm uart3.elf
